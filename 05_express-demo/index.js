f710a921e88524d0b1d1f77d59e2378da4143acf
Hebert Hendrik <hebert.hendrik@gmail.com>
2020-07-16 14:08:07 +0700

    Update index.js

--- 05_express-demo/index.js
+++ 05_express-demo/index.js
@@ -5,6 +5,11 @@
 // export PORT=5000
 // set PORT=5000
 
+// app.get()    --> ambil data
+// app.post()   --> create data
+// app.put()    --> update data
+// app.delete() --> delete data
+
 const express = require('express');
 const app = express();
 
@@ -16,11 +21,21 @@ const app = express();
 */
 
 // HTTP REQUEST HANDLER DI ROOT
+
+
+/*
+NOTE
+1. dalam 1 rute, kalian hanya bisa punya 1 respond.
+2. bayangkan ROUTE adalah ibarat jabat tangan
+3. HTTP request dikirim ke server, bayangkan itu adalah orang yang mau jabat tangan sama kita
+4. Saat kita sudah jabat tangan dengan orang tersebut, maka klo ada orang lain yang 
+mau jabat tangan juga dengan kalian, tidak bisa.
+*/
 app.get("/", (req, res) => {
     res.send('Hello World!');
 });
 
-// HTTO REQUEST HANDLER DI /API/COURSES
+// HTTP REQUEST HANDLER DI /API/COURSES
 app.get("/api/courses", (req, res) => {
     res.send([1, 2, 3]);
 });
