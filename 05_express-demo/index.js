26f940378bc484abc4ecd7a118a87126086ff590
Hebert Hendrik <hebert.hendrik@gmail.com>
2020-07-16 16:21:09 +0700

    Update index.js

--- 05_express-demo/index.js
+++ 05_express-demo/index.js
@@ -13,6 +13,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 
 /*
 *
@@ -39,10 +41,11 @@ const courses = [
     { id: 3, name: 'course3' }
 ];
 
-
+/*
 app.get("/", (req, res) => {
     res.send('Hello World!');
 });
+*/
 
 // HTTP REQUEST HANDLER DI /API/COURSES
 app.get("/api/courses", (req, res) => {
@@ -56,6 +59,24 @@ app.get("/api/courses/:id", (req, res) => {
     if( !course ) res.status(404).send('The course with the given ID was not found.');
     res.send(course);
 });
+
+// POST -> gunanya untuk create data
+app.post("/api/courses", (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    // res.send(course);
+    return null;
+});
+
+
+
+
+
+
+
 
 
 
