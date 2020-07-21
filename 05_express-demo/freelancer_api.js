50f0930228209765f53af481e7204d6bee21da41
Hebert Hendrik <hebert.hendrik@gmail.com>
2020-07-21 16:12:23 +0700

    update

--- 05_express-demo/freelancer_api.js
+++ 05_express-demo/freelancer_api.js
@@ -38,6 +38,53 @@ app.get('/api/users', (req, res) => {
 
 });
 
+app.get('/api/users/:email/:password', (req, res) => {
+
+    // LOG TIME
+    var datetime = new Date();
+    console.log("\n"+datetime);
+    console.log("Incoming new GET HTTP request for LOGIN");
+    console.log(req.body);
+
+    // VALIDATE
+    const {error} = validateUser(req.body);
+    if (error) {
+        console.log('Validation error');
+
+        var jsonRespond = {
+            result: "",
+            message: error.details[0].message
+        }
+
+        return res.status(400).json(jsonRespond);
+    }
+    console.log('Validation success and accepted');
+
+    // CHECK IF THE EMAIL AND PASSWORD CORRECT
+    console.log('Check existing email: '+req.params.email+' and password: '+req.params.password);
+    const check_user = users.find( u => u.email === req.params.email && u.password === req.params.email );
+    if (!check_user) {
+        var error_message = 'Invalid login detail. Email or password is not correct.';
+        console.log(error_message);
+        
+        var jsonRespond = {
+            result: "",
+            message: error_message
+        }
+
+        return res.status(404).json(jsonRespond);
+    }
+
+    var jsonRespond = {
+        result: user,
+        message: "Login success"
+    }
+    return res.json(jsonRespond);
+
+
+
+});
+
 // REGISTER NEW USER
 app.post('/api/users', (req, res) => {
 
@@ -62,14 +109,14 @@ app.post('/api/users', (req, res) => {
     console.log('Validation success and accepted');
 
     // CHECK IF THE EMAIL ALREADY EXISTS
-    console.log('Check existing email: '+req.body.email)
+    console.log('Check existing email: '+req.body.email);
     const check_user = users.find( u => u.email === req.body.email );
     if (check_user) {
         console.log('Email: '+req.body.email+' is already registered');
         
         var jsonRespond = {
             result: "",
-            message: "Registration failed. Email "+req.params.email+" is already registered. Please use other email."
+            message: "Registration failed. Email "+req.body.email+" is already registered. Please use other email."
         }
 
         return res.status(404).json(jsonRespond);
