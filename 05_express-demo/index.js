cc0f2cf11dd40dc6c90d713e4d7aeb91e1e89448
Hebert Hendrik <hebert.hendrik@gmail.com>
2020-07-21 12:43:23 +0700

    Update index.js

--- 05_express-demo/index.js
+++ 05_express-demo/index.js
@@ -3,20 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
-
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/*
-*
-* LOCAL VARILABLE, DATABASE COURSE
-*
-*/
-
+// TEMPORARY VARILABLE
 const courses = [
     { id: 1, name: 'Matematika' },
     { id: 2, name: 'Kimia' },
@@ -24,129 +17,77 @@ const courses = [
     { id: 4, name: 'Web Programming' }
 ];
 
-/*
-app.get("/", (req, res) => {
-    res.send('Hello World!');
-});
-*/
 
-
-/*
-*
-* WEB SERVICE UNTUK RETURN ALL COURSES
-*
-*/
+// GET ALL COURSE
 app.get("/api/courses", (req, res) => {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.send( courses );
 });
 
-/*
-* 
-* WEB SERVICE UNTUK RETURN 1 OBJECT COURSE BY COURSE ID
-*
-*/
+// GET 1 COURSE DETAIL
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find( c => c.id === parseInt(req.params.id) );
     if( !course ) res.status(404).send('The course with the given ID was not found.');
     res.send(course);
 });
 
-/*
-*
-* WEB SERVICE UNTUK ADD 1 DATA BARU KE DALAM MASTER DATA COURSES
-*
-*/
+// ADD NEW COURSE
 app.post("/api/courses", (req, res) => {
 
-
-    /*
-    * 
-    * SCHEMA UNTUK VALIDASI OBJECT YANG MASUK KE HTTP REQUEST
-    *
-    */
+    // VALIDASI USING SCHEMA
     const {error} = validateCourse(req.body); 
     if( error ){
-        // 400 Bad Request
         return res.status(400).send(error.details[0].message);
     } 
 
-
+    // ADD NEW COURSE
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
-    res.send(course);
-
+    return res.send(course);
 });
 
-/*
-*
-* WEB SERVICE UNTUK UPDATE 1 COURSE BASED ON COURSE ID
-*
-*/
+// UPDATE COURSE BY ID
 app.put('/api/courses/:id', (req, res) => {
 
-    // validasi
-    // klo ga sesuai, return 400
+    // VALIDASI USING SCHEMA
     const {error} = validateCourse(req.body); 
     if( error ){
         // 400 Bad Request
         return res.status(400).send(error.details[0].message);
     } 
 
-    // cari course nya
-    // klo ga ketemu, return 404
-    const course = courses.find( c => c.id === parseInt(req.params.id) );
-    if( !course ) res.status(404).send('The course with the given ID was not found.');
-
-    // klo semuanya sesuai dengan persyaratan
-    // maka update course nya
-    // return object course yang baru setelah di update
-    course.name = req.body.name;
-    res.send(course);
-
-});
-
-
-
-
-
-
-/*
-*
-* WEB SERVICE UNTUK DELETE 1 COURSE BASED ON COURSE ID
-*
-*/
-app.delete('/api/courses/:id', (req, res) => {
-
-    // cari course nya
-    // klo ga ketemu, return 404 
+    // FIND MATCH COURSE
     const course = courses.find( c => c.id === parseInt(req.params.id) );
     if( !course ) return res.status(404).send('The course with the given ID was not found.');
 
-    // delete
+    // UPDATE COURSE
+    course.name = req.body.name;
+    return res.send(course);
+
+});
+
+// DELETE COURSE BY ID
+app.delete('/api/courses/:id', (req, res) => {
+
+    // FIND MATCH COURSE
+    const course = courses.find( c => c.id === parseInt(req.params.id) );
+    if( !course ) return res.status(404).send('The course with the given ID was not found.');
+
+    // DELETE COURSE
     const index = courses.indexOf(course);
     courses.splice(index, 1);
 
     console.log("Course ID "+req.params.id+" telah berhasil dihapus");
 
-    // return course yang berhasil dihapus
+    // RETURN DELETED COURSE
     return res.send(course);
-
 });
 
-
-
-
-
-
-
-
+// FUNCTION TO VALIDATE COURSE
 function validateCourse(course){
     const schema = Joi.object({
         name: Joi.string().min(3).required()
@@ -155,28 +96,7 @@ function validateCourse(course){
     return schema.validate(course);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-*
-* START WEB SERVICE
-*
-*/
-
+// START WEB SERVICE
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
